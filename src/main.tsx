3b73a7e869e5deaf2156df23a751461d8f8fa311
Arden <arden.haldorson@gmail.com>
2023-08-25 14:54:24 +0200

    organised with folders, added editorconfig, cleaned up unc files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
-import './index.css'
+import {
+	QueryClient,
+	QueryClientProvider
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			staleTime: 1000 * 60 * 5, // 5 minutes -- global, def 0
+			cacheTime: 1000 * 60 * 60, // 1 hour
+		}
+	}
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</QueryClientProvider>
+	</React.StrictMode>
 )
