2ed2fc6567346a19585d22a84d37ac2faf604c3d
Arden <arden.haldorson@gmail.com>
2023-08-30 16:39:05 +0200

    testing diff apperances of card for movies

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -2,7 +2,7 @@ import { Movie } from '../types/TMDB.types'
 import Alert from 'react-bootstrap/Alert'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
-import CardGroup from 'react-bootstrap/CardGroup'
+import ListGroup from 'react-bootstrap/ListGroup';
 import React from 'react'
 import Row from 'react-bootstrap/Row'
 
@@ -18,7 +18,7 @@ const MovieGrid: React.FC<IProps> = ({ data }) => {
 	}
 
 	return (
-		<Row xs={1} md={3} lg={4} className='g-3 movie-grid'>
+		<Row xs={1} sm={3} md={3} lg={4} className='g-3 movie-grid'>
 			{data.map(movie => (
 				<Card
 					bg='dark'
@@ -27,14 +27,25 @@ const MovieGrid: React.FC<IProps> = ({ data }) => {
 					text='light'
 				>
 					<Card.Img variant='' src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-					<Card.ImgOverlay >{movie.vote_average}</Card.ImgOverlay>
+					<Card.ImgOverlay>
+						<Card.Title>{movie.title}</Card.Title>
+						{/* <Card.Text>
+							{movie.overview}
+						</Card.Text> */}
+						{/* <Button variant="primary">Go somewhere</Button> */}
+					</Card.ImgOverlay>
+					{/* <Card.ImgOverlay >{movie.vote_average}</Card.ImgOverlay>
 					<Card.Body>
 						<Card.Title>{movie.title}</Card.Title>
 						<Card.Text>
 							{movie.overview}
 						</Card.Text>
 						<Button variant="primary">Go somewhere</Button>
-					</Card.Body>
+					</Card.Body> */}
+					<ListGroup className="list-group-flush">
+						<ListGroup.Item>{movie.title}</ListGroup.Item>
+						<ListGroup.Item>Release date: {movie.release_date}</ListGroup.Item>
+					</ListGroup>
 				</Card>
 			))}
 		</Row>
