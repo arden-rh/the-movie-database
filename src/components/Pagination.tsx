4af7474b180ef8cd60706f65a4a75afc299b1bdd
Arden <arden.haldorson@gmail.com>
2023-09-18 03:35:10 +0200

    tried using a hook for fetching movie data by genre but couldn't make it work, redid the thing

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface IProps {
+
+}
+
+const Pagination : React.FC<IProps> = ({}) => {
+
+	return (
+		<div>Pagination</div>
+	)
+}
+
+export default Pagination
--- src/pages/MovieGenrePage.tsx
+++ src/pages/MovieGenrePage.tsx
@@ -1,50 +1,106 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Link, useNavigate, useParams } from "react-router-dom"
 import MovieGrid from '../components/MovieGrid'
 import { useQuery } from '@tanstack/react-query'
-import { getMoviesByGenre } from '../services/TMDB_API'
+import { getMovieGenres, getMoviesByGenre } from '../services/TMDB_API'
 import { Movie_Genre, Movie_Results } from '../types/TMDB.types'
 import useMovieGenres from '../hooks/useMovieGenres'
+import useMoviesByGenre from '../hooks/useMoviesByGenre'
+import useMoviesByGenreCopy from '../hooks/useMoviesByGenre-copy'
 
 
 const MovieGenrePage = () => {
 
 	const { genre } = useParams()
+	const [movieGenreId, setMovieGenreId] = useState<number>(0)
 	const [movieGenre, setMovieGenre] = useState<string>('')
-	const [movieGenres, setMovieGenres] = useState<Movie_Genre[] | null>(null)
+	// const [movies, setMovies] = useState<Movie_Results | null>(null)
+	const [loadingMovies, setLoadingMovies] = useState(false)
+	// const [movieGenres, setMovieGenres] = useState<Movie_Genre[] | null>(null)
 
 	const [movieData, setMovieData] = useState<Movie_Results | null>(null)
 
-	const useMoviesByGenre = (genre: number, page: number) => {
-		return useQuery(['movies_by_genre'], () => getMoviesByGenre(genre, page))
-	}
-
 	const { data } = useMovieGenres()
 
-	if (!data) {
-		return <p>No genres found</p>
-	}
+	const findGenreId = useCallback((data: Movie_Genre[]) => {
 
-	setMovieGenres(data?.genres)
+		try {
 
-	const chosenGenre = movieGenres?.find( movieGenre => {
-		movieGenre.name === genre
+			if (!data) {
+				throw new Error('No genres found')
+			}
+
+			const chosenGenre = data.find((movie) => {
+				return movie.name.toLowerCase().split(" ").join('-') === movieGenre
+			})
+
+			if (!chosenGenre) {
+				throw new Error('The chosen genre was not found')
+			}
+
+			// setMovieGenreId(chosenGenre.id)
+			return chosenGenre.id
+
+
+		} catch (e) {
+			console.log('error', e)
+		}
+
+	}, [movieGenre])
+
+
+	const { data: movies, isError, isLoading, refetch } = useQuery({
+		queryKey: ['movies_by_genre'],
+		queryFn: () => getMoviesByGenre(movieGenreId, 1),
+		enabled: !!movieGenreId
 	})
 
-	// console.log(movieGenres!.genres)
 
-	// if (!chosenGenre) {
-	// 	return
-	// }
+	useEffect(() => {
 
-	const { data: movies } = useMoviesByGenre(chosenGenre?.id, 1)
+		console.log('fire useeffect')
 
+		setLoadingMovies(true)
+
+		try {
+
+			if (!data) {
+				throw new Error('Unable to find data')
+			}
+
+			const id = findGenreId(data.genres)
+			console.log(id)
+			// findGenreId(data.genres)
+
+			if (id) {
+				setMovieGenreId(id)
+			}
+
+		} catch (e) {
+
+			console.log('error', e)
+
+		}
+
+		setLoadingMovies(false)
+
+	}, [data, findGenreId])
+
+	useEffect(() => {
+
+		console.log('firing use effect genre')
+		setLoadingMovies(true)
+		setMovieGenre(genre as string)
+
+	}, [genre])
 
 	return (
 		<div>
 			<h1>Genre: <span className='text-capitalize'>{genre}</span></h1>
 
-			{/* {data && <MovieGrid data={data.results} />} */}
+			{isLoading || loadingMovies && <span className='text-capitalize'>Loading...</span>}
+
+			{!loadingMovies && movies && <MovieGrid data={movies.results} />}
 
 		</div>
 	)
