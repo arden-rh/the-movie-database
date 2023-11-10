6f6dc2e36801d11fcdce690b0817fc427714e0bf
Arden <arden.haldorson@gmail.com>
2023-11-10 21:59:05 +0100

    wip: continued styling, link to single movie pages on cards

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,10 +1,10 @@
 import { Movie } from '../types/TMDB_Movie.types'
+import { NavLink, Link } from 'react-router-dom'
 import Alert from 'react-bootstrap/Alert'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
-import ListGroup from 'react-bootstrap/ListGroup';
-import React from 'react'
 import Row from 'react-bootstrap/Row'
+
 
 
 interface IProps {
@@ -20,25 +20,27 @@ const MovieGrid: React.FC<IProps> = ({ data }) => {
 	return (
 		<Row xs={1} sm={3} md={4} lg={5} className='g-3 movie-grid'>
 			{data.map(movie => (
-				<Card
-					key={movie.id}
-				>
-					<Card.Img variant='' src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-					<div className="card-info">
-						<div>
-							<span>Title:</span>
-							{movie.title}
+				<Link to={`/movie/${movie.id}`}>
+					<Card
+						key={movie.id}
+					>
+						<Card.Img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+						<div className="card-info">
+							<div>
+								<span>Title:</span>
+								{movie.title}
+							</div>
+							<div>
+								<span>Release date:</span>
+								{movie.release_date}
+							</div>
+							<div>
+								<span>Average vote:</span>
+								{movie.vote_average.toFixed(1)}
+							</div>
 						</div>
-						<div>
-							<span>Release date:</span>
-							{movie.release_date}
-						</div>
-						<div>
-							<span>Average vote:</span>
-							{movie.vote_average.toFixed(1)}
-						</div>
-					</div>
-				</Card>
+					</Card>
+				</Link>
 			))}
 		</Row>
 	)
