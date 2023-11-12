73132f3784a32f77b694ef62f7d0cd09abe55887
Arden <arden.haldorson@gmail.com>
2023-11-12 23:37:30 +0100

    cleaned up css

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -6,7 +6,7 @@ const NotFoundPage = () => {
 	const navigate = useNavigate()
 
 	return (
-		<div className="page-container">
+		<div className="page-container not-found-container">
 			<div className='category-page-hero'>
 				<h1>404: Not Found</h1>
 			</div>
